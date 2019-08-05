35311603b9a6e2a8ef5a447b446efbb4218b6f8f
xuchaoa <xuchaoao@outlook.com>
2019-08-05 11:29:21 +0800

    解决xpoc异步扫描问题

--- ScanMoudle/xscan-poc/xpoc.py
+++ ScanMoudle/xscan-poc/xpoc.py
@@ -7,7 +7,8 @@
 # modify from poc-T Proj by Archerx
 # xpoc将作为一个单独可运行的组件，和主调度引擎之间耦合度很小,方便单独拿出来用
 
-
+from gevent import monkey
+monkey.patch_all()
 import os
 from lib.core.common import set_paths
 from lib.core.data import scan_option
@@ -21,7 +22,7 @@ def main():
     try:
         set_paths(module_path())
 
-        x = {'engine_thread': True, 'concurrent_num': 100, 'poc_name': 'weblogic_2019_48814',
+        x = {'engine_thread': False, 'concurrent_num': 100, 'poc_name': 'weblogic_2019_48814',
              'target_single': '', 'target_range': '', 'target_network': '', 'zoomeye_dork': 'weblogic',
              'shodan_dork': '', 'fofa_dork': '', 'censys_dork': '', 'api_limit': 50, 'api_offset': 0, 'search_type': 'host',
              'output_path': '', 'logging_level': 0, 'proxy': ''}
